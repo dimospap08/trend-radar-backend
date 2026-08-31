7b796d24446386489dce3a90940028042a1b9de7
dimospapadopoulos08 <dimospapadopoulos08@gmail.com>
2026-08-31 17:32:11 +0300

    fix: restore new Trend Radar frontend design

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TrendRadar from "./TrendRadar.jsx";
+import TrendRadar from "../TrendRadar-real-media-update.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
